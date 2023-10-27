5ba83c7e8b035780670f0bcc57ed449f22486edf
Turan Balayev <turanbalayev96@gmail.com>
2023-10-27 15:17:04 +0400

    Implement search products by category id feature

--- src/main/java/com/turanbalayev/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/turanbalayev/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.turanbalayev.ecommerce.dao;
 
 import com.turanbalayev.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+
+@CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository  extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
 }
